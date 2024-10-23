9f38ac43a7907e79dbe1f1b2f64188bc70393e32
Kolapo Wariz <kolapowariz@gmail.com>
2024-10-23 20:28:32 +0100

    UserList test

---
+++ test/components/UserList.test.tsx
@@ -0,0 +1,31 @@
+import { it, expect, describe } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/vitest'
+import React from 'react'
+import UserList from '../../src/components/UserList'
+import { User } from '../../src/entities'
+
+describe('UserList', () => {
+  it('should not render user list when array is empty', () => {
+
+    render(<UserList users={[]} />)
+
+    expect(screen.getByText(/no user/i)).toBeInTheDocument()
+  });
+
+  it('should render list of user', () => {
+    const users: User[] = [
+      { id: 1, name: 'wariz' },
+      { id: 2, name: 'Dimeji' }
+    ]
+
+    render(<UserList users={users} />)
+
+    users.forEach(user => {
+      const link = screen.getByRole('link', { name: user.name});
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute('href', `/users/${user.id}`)
+    })
+
+  })
+})
