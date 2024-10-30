728f00d89af44dd11555d33dbdb7ed86af087c30
Kolapo Wariz <kolapowariz@gmail.com>
2024-10-30 11:30:52 +0100

    Expandable Text

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,8 +1,11 @@
 // import Onboarding from "../components/Onboarding";
-import TermsAndConditions from "../components/TermsAndConditions";
+// import TermsAndConditions from "../components/TermsAndConditions";
+
+import ExpandableText from "../components/ExpandableText";
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />
+  const longText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, animi repellat? Magnam, animi eaque vitae at soluta officiis laudantium ratione? Beatae facilis voluptates, ipsam sint repudiandae similique molestiae doloremque impedit ut dolorum veniam temporibus dolor velit sapiente labore numquam corrupti officiis ullam placeat reprehenderit perferendis tempora? Ratione reprehenderit excepturi corrupti nobis sequi et necessitatibus eveniet consequuntur ipsam iusto sunt reiciendis nostrum, aspernatur qui ipsa dolores! Optio adipisci ab inventore ad minus nulla tempora repellendus sit eaque omnis? Fuga voluptatem autem ipsam asperiores minima molestias, saepe eveniet rerum dolor tenetur nemo ipsum velit maxime magni. Ab similique enim aspernatur aut sed.'
+  return <ExpandableText text={longText} />
 };
 
 export default PlaygroundPage;
