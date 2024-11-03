c0b5ce2a6babcca0e6a8fa41ca6eff76df6c08eb
Kolapo Wariz <kolapowariz@gmail.com>
2024-11-03 09:59:22 +0100

    SearchBox test case

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,11 +1,7 @@
-// import Onboarding from "../components/Onboarding";
-// import TermsAndConditions from "../components/TermsAndConditions";
-
-import ExpandableText from "../components/ExpandableText";
+import SearchBox from "../components/SearchBox";
 
 const PlaygroundPage = () => {
-  const longText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, animi repellat? Magnam, animi eaque vitae at soluta officiis laudantium ratione? Beatae facilis voluptates, ipsam sint repudiandae similique molestiae doloremque impedit ut dolorum veniam temporibus dolor velit sapiente labore numquam corrupti officiis ullam placeat reprehenderit perferendis tempora? Ratione reprehenderit excepturi corrupti nobis sequi et necessitatibus eveniet consequuntur ipsam iusto sunt reiciendis nostrum, aspernatur qui ipsa dolores! Optio adipisci ab inventore ad minus nulla tempora repellendus sit eaque omnis? Fuga voluptatem autem ipsam asperiores minima molestias, saepe eveniet rerum dolor tenetur nemo ipsum velit maxime magni. Ab similique enim aspernatur aut sed.'
-  return <ExpandableText text={longText} />
+  return <SearchBox onChange={(text) => console.log(text) }/>
 };
 
 export default PlaygroundPage;
