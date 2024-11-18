d40ac82ce19b16497fd5b026040caf13b1fac31b
Kolapo Wariz <kolapowariz@gmail.com>
2024-11-18 15:21:29 +0100

    Chapter 4, Testing focus

---
+++ test/components/ProductForm.test.tsx
@@ -0,0 +1,68 @@
+import { render, screen } from '@testing-library/react'
+import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest'
+import ProductForm from '../../src/components/ProductForm'
+import { Category, Product } from '../../src/entities'
+import AllProviders from '../AllProvider'
+import { db } from '../mocks/db'
+
+describe('ProductForm', () => {
+
+  let category: Category;
+
+  beforeAll(() => {
+    category = db.category.create()
+  })
+
+  afterAll(() => {
+    db.category.delete({ where: { id: { equals: category.id } } })
+  })
+
+  const renderComponent = (product?: Product) => {
+    render(<ProductForm product={product} onSubmit={vi.fn()} />, { wrapper: AllProviders })
+
+    return {
+      waitForFormToLoad: async () => {
+        await screen.findByRole('form');
+        return {
+          nameInput: screen.getByPlaceholderText(/name/i),
+          priceInput: screen.getByPlaceholderText(/price/i),
+          categoryInput: screen.getByRole('combobox', { name: /category/i })
+        }
+      }
+    }
+  }
+  it('should render form fields', async () => {
+
+    const { waitForFormToLoad } = renderComponent()
+
+    const { nameInput, categoryInput, priceInput } = await waitForFormToLoad();
+
+    expect(nameInput).toBeInTheDocument()
+    expect(priceInput).toBeInTheDocument()
+    expect(categoryInput).toBeInTheDocument()
+  });
+
+
+  it('should populate form fields when editing a product', async () => {
+    const product: Product = {
+      id: 1,
+      name: 'Bread',
+      price: 10,
+      categoryId: category.id
+    }
+    const { waitForFormToLoad} = renderComponent(product)
+
+    const inputs = await waitForFormToLoad()
+
+    expect(inputs.nameInput).toHaveValue(product.name)
+    expect(inputs.priceInput).toHaveValue(product.price.toString())
+    expect(inputs.categoryInput).toHaveTextContent(category.name)
+  });
+
+  it('should put focus on the name field', async () => {
+    const { waitForFormToLoad } = renderComponent()
+
+    const { nameInput } = await waitForFormToLoad()
+    expect(nameInput).toHaveFocus()
+  })
+})
