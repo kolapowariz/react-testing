2b938b4ed3723192f1c6a336484a7671fa5eb06f
Kolapo Wariz <kolapowariz@gmail.com>
2024-11-23 21:18:39 +0100

    Chapter 7

--- test/setup.ts
+++ test/setup.ts
@@ -1,13 +1,24 @@
+import "@testing-library/jest-dom/vitest";
+import { PropsWithChildren } from "react";
+import ResizeObserver from "resize-observer-polyfill";
 import { afterAll, afterEach, beforeAll, vi } from "vitest";
-import '@testing-library/jest-dom/vitest'
-import ResizeObserver from 'resize-observer-polyfill'
 import { server } from "./mocks/server";
 
-beforeAll(()=> server.listen())
-afterEach(()=> server.resetHandlers());
-afterAll(()=> server.close())
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
-vi.mock('@auth0/auth0-react')
+vi.mock("@auth0/auth0-react", () => {
+  return {
+    useAuth0: vi.fn().mockReturnValue({
+      isAuthenticated: false,
+      isLoading: false,
+      user: undefined,
+    }),
+    Auth0Provider: ({ children }: PropsWithChildren) => children,
+    withAuthenticationRequired: vi.fn(),
+  };
+});
 
 global.ResizeObserver = ResizeObserver;
 
@@ -15,10 +26,9 @@ window.HTMLElement.prototype.scrollIntoView = vi.fn();
 window.HTMLElement.prototype.hasPointerCapture = vi.fn();
 window.HTMLElement.prototype.releasePointerCapture = vi.fn();
 
-
-Object.defineProperty(window, 'matchMedia', {
+Object.defineProperty(window, "matchMedia", {
   writable: true,
-  value: vi.fn().mockImplementation(query => ({
+  value: vi.fn().mockImplementation((query) => ({
     matches: false,
     media: query,
     onchange: null,
@@ -27,5 +37,5 @@ Object.defineProperty(window, 'matchMedia', {
     addEventListener: vi.fn(),
     removeEventListener: vi.fn(),
     dispatchEvent: vi.fn(),
-  }))
+  })),
 });
--- test/utils.tsx
+++ test/utils.tsx
@@ -2,6 +2,9 @@ import { useAuth0, User } from "@auth0/auth0-react";
 import { delay, http, HttpResponse } from "msw";
 import { vi } from "vitest";
 import { server } from "./mocks/server";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import routes from "../src/routes";
+import { render} from '@testing-library/react'
 
 export const simulateDelay = (endpoint: string) => {
   server.use(
@@ -34,3 +37,11 @@ export const mockAuthState = (authState: AuthState) => {
     handleRedirectCallback: vi.fn(),
   });
 };
+
+export const navigateTo = (path: string) => {
+  const router = createMemoryRouter(routes, {
+    initialEntries: [path]
+  })
+
+  render(<RouterProvider router={router} />)
+}
